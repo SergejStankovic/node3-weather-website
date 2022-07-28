bb21997b599cc1644c6233a1d8fa839c8c9fd784
Sergius <sergejstankovic@gmail.com>
2022-07-28 23:04:22 +0200

    Add info about UV index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const os = require('os')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=0e286d94df2e1afb4872a48512683751&query=' + latitude + ',' + longitude + '&units=m'
@@ -9,7 +10,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " °C out. Feelslike: " + response.body.current.feelslike + " °C.")
+            callback(undefined, response.body.current.weather_descriptions[0] + 
+                ". It is currently " + response.body.current.temperature + " °C out. Feelslike: " 
+                + response.body.current.feelslike + " °C. UV Index: " + response.body.current.uv_index)
         }
     })
 }
